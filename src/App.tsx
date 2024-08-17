2b28cea9b34fb654236a340c6e5794024a025d30
David Aarlien <davidaarlien@hotmail.com>
2024-08-17 23:27:43 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Header from "./components/base/Header";
+import "./App.css";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </Main>
+      </Router>
+    </>
+  );
+}
+
+const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 2rem;
+`;
+
+export default App;
---
+++ src/components/base/Header.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <LogoWrapper>
+        <span>AAR</span>
+        <span>.DEV</span>
+      </LogoWrapper>
+      <nav>
+        <List>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </List>
+      </nav>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 75rem;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+`;
+
+const LogoWrapper = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  line-height: 1.875rem;
+  font-size: 2rem;
+  font-weight: bold;
+
+  span:first-child {
+    color: #f96d00;
+  }
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+
+  li {
+    margin-right: 0.625rem;
+    font-size: 1.125rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default Header;
